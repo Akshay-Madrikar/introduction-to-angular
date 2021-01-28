cd2de2b1690c9d965c85be602c4a765f0d42dab1
Akshay-Madrikar <akshaymadrikar@gmail.com>
2021-01-28 23:38:50 +0530

    angular in action

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
+import { AppRoutingModule } from './app-routing.component';
+
+@NgModule({
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PersonsService } from './persons.service';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+})
+export class PersonsComponent implements OnInit, OnDestroy {
+  isFetching = false;
+  personList: string[];
+  private personListSubs: Subscription;
+
+  constructor(private prsService: PersonsService) {}
+
+  ngOnInit() {
+    this.personListSubs = this.prsService.personsChanged.subscribe(
+      (persons) => {
+        this.personList = persons;
+        this.isFetching = false;
+      }
+    );
+    this.isFetching = true;
+    this.prsService.fetchPersons();
+  }
+
+  onRemovePerson(personName: string) {
+    this.prsService.removePerson(personName);
+  }
+
+  ngOnDestroy() {
+    this.personListSubs.unsubscribe();
+  }
+}
